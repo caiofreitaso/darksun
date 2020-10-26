996f87eeb93c2ca311dd0c8d6bdc68dab63306ef
Caio Freitas de Oliveira <caiofreitaso@gmail.com>
2020-10-26 19:17:12 -0300

    advance background.todo

--- update-spells.sh
+++ update-spells.sh
@@ -3,6 +3,7 @@
 SPELLS=sections/spells/spells.tex
 
 echo "\section{Spell Descriptions}" > $SPELLS
+echo "The spells herein are presented in alphabetical order (with the exception of those whose names begin with ``greater,'' ``lesser,'' or ``mass''; see Order of Presentation)." >> $SPELLS
 echo "" >> $SPELLS
 find subsections/spells/ -type f | sort | awk '{print "\\input{" $0 "}"}' >> $SPELLS
 ./build.sh
