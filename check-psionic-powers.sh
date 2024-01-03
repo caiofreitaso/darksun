2b501d0c48bda05113ce0fcae3d61531b71bfad8
Caio Freitas de Oliveira <caio@debian-BULLSEYE-live-builder-AMD64>
2024-01-03 19:06:14 -0300

    add new powers; fix 'see text'; add glamer and figment as subdisciplines

--- check-psionic-powers.sh
+++ check-psionic-powers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-vimdiff <(find subsections/powers/ -name "*.tex" | sort) <(awk '{ if($0 ~ /input/) { print substr($0,8) } }' sections/powers/powers.tex | rev | cut -c 2- | rev)
+vimdiff <(find subsections/powers/ -name "*.tex" | sort | sed 's/-/ /g') <(awk '{ if($0 ~ /input/) { print substr($0,8) } }' sections/powers/powers.tex | rev | cut -c 2- | rev | sed 's/-/ /g')
