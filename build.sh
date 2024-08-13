7cc8d457e9271b7f11041aa6d7254073886d269a
Caio Freitas de Oliveira <caio.freitaso@gmail.com>
2024-08-13 17:45:20 -0300

    fix shell scripts; fix druid

--- build.sh
+++ build.sh
@@ -9,39 +9,94 @@ function create_pdf(){
   rubber --clean $1
 }
 
+function usage() {
+  echo "Usage: ${0} [-cilosu]"
+  echo -e '\t-c\tCreate dark-sun.pdf'
+  echo -e '\t-i\tOptimize images in ./images/'
+  echo -e '\t-l\tCreate low resolution PDF (name: dark-sun.<current_date>.pdf)'
+  echo -e '\t-o\tOptimize dark-sun.pdf'
+  echo -e '\t-s\tSplit dark-sun into different parts'
+  echo -e '\t-u\tUpdate external dependencies for pdfsizeopt'
+
+  exit 1
+}
+
 ############
 ### MAIN ###
 ############
 
 CURRENT_FILENAME="dark-sun.$(date +"%Y-%m-%dT%H-%M").pdf"
 
-./scripts/optimize-bw-images.sh &
-./scripts/update-pdfsizeopt.sh &
-create_pdf credits-and-legal &
-create_pdf ogl &
-wait
+while getopts 'cilosu' OPTION
+do
+  case "${OPTION}" in
+    c)
+      CREATE_PDF=1;;
+    i)
+      OPTIMIZE_IMAGE=1;;
+    l)
+      LOW_RES=1;;
+    o)
+      OPTIMIZE_PDF=1;;
+    s)
+      SPLIT_PDF=1;;
+    u)
+      UPDATE_PDFSIZEOPT=1;;
+    *)
+      usage;;
+  esac
+done
 
-create_pdf dark-sun
+if [[ ${OPTIMIZE_IMAGE} ]]
+then
+  ./scripts/optimize-bw-images.sh &
+fi
+
+if [[ ${UPDATE_PDFSIZEOPT} ]]
+then
+  ./scripts/update-pdfsizeopt.sh &
+fi
+
+if [[ ${CREATE_PDF} ]]
+then
+  create_pdf credits-and-legal &
+  create_pdf ogl &
+  wait
+
+  create_pdf dark-sun
+fi
+
 pdftk dark-sun.pdf dump_data \
   | grep -e Bookmark \
-  | sed 's/&apos;/’/g' > dark-sun.bookmarks
+  | sed 's/(&apos;|&#8217;)/’/g' > dark-sun.bookmarks
 
-./scripts/optimize-pdf.sh dark-sun "${CURRENT_FILENAME}" &
-./scripts/create-low-res.sh dark-sun &
+if [[ ${OPTIMIZE_PDF} ]]
+then
+  ./scripts/optimize-pdf.sh dark-sun "${CURRENT_FILENAME}" &
+fi
 
-PHB_PAGE=$(./scripts/find-part-page.sh "Player’s Handbook" I)
-ITEMS_PAGE=$(./scripts/find-part-page.sh "Athasian Emporium" II)
-MAGIC_PAGE=$(./scripts/find-part-page.sh "Universae Grimoire" III)
-PSIONICS_PAGE=$(./scripts/find-part-page.sh "The Will and The Way" IV)
-WORLD_PAGE=$(./scripts/find-part-page.sh "Wanderer’s Journal" V)
-DM_PAGE=$(./scripts/find-part-page.sh "Dungeon Master’s Guide" VI)
+if [[ ${LOW_RES} ]]
+then
+  ./scripts/create-low-res.sh dark-sun &
+fi
 
-./scripts/split-pdf.sh dark-sun ${PHB_PAGE} ${ITEMS_PAGE} 1_players &
-./scripts/split-pdf.sh dark-sun ${ITEMS_PAGE} ${MAGIC_PAGE} 2_items &
-./scripts/split-pdf.sh dark-sun ${MAGIC_PAGE} ${PSIONICS_PAGE} 3_magic &
-./scripts/split-pdf.sh dark-sun ${PSIONICS_PAGE} ${WORLD_PAGE} 4_psionics &
-./scripts/split-pdf.sh dark-sun ${WORLD_PAGE} ${DM_PAGE} 5_wanderer &
-./scripts/split-pdf.sh dark-sun ${DM_PAGE} end 6_dungeon-master &
+if [[ ${SPLIT_PDF} ]]
+then
+  PHB_PAGE=$(./scripts/find-part-page.sh "Player\(’\|\&#8217;\)s Handbook" I)
+  ITEMS_PAGE=$(./scripts/find-part-page.sh "Athasian Emporium" II)
+  MAGIC_PAGE=$(./scripts/find-part-page.sh "Universae Grimoire" III)
+  PSIONICS_PAGE=$(./scripts/find-part-page.sh "The Will and The Way" IV)
+  WORLD_PAGE=$(./scripts/find-part-page.sh "Wanderer\(’\|\&#8217;\)s Journal" V)
+  DM_PAGE=$(./scripts/find-part-page.sh "Dungeon Master\(’\|\&#8217;\)s Guide" VI)
+
+  ./scripts/split-pdf.sh dark-sun ${PHB_PAGE} ${ITEMS_PAGE} 1_players &
+  ./scripts/split-pdf.sh dark-sun ${ITEMS_PAGE} ${MAGIC_PAGE} 2_items &
+  ./scripts/split-pdf.sh dark-sun ${MAGIC_PAGE} ${PSIONICS_PAGE} 3_magic &
+  ./scripts/split-pdf.sh dark-sun ${PSIONICS_PAGE} ${WORLD_PAGE} 4_psionics &
+  ./scripts/split-pdf.sh dark-sun ${WORLD_PAGE} ${DM_PAGE} 5_wanderer &
+  ./scripts/split-pdf.sh dark-sun ${DM_PAGE} end 6_dungeon-master &
+fi
+
 wait
 
 rm *.bookmarks
--- scripts/split-pdf.sh
+++ scripts/split-pdf.sh
@@ -22,7 +22,6 @@ function get_bookmark_line() {
 
 function get_end_bookmark() {
   local end="${1}"
-  local end_bookmark=$(( $(get_bookmark_line "${end}") - 4 ))
   
   if [[ "${end}" != "end" ]]
   then
@@ -32,33 +31,49 @@ function get_end_bookmark() {
   echo "${end}"
 }
 
+function create_bookmarks() {
+  local output="${1}"
+  local start="${2}"
+  local end="${3}"
+
+  log [${output}] Creating bookmarks
+
+  local start_bookmark=$(( $(get_bookmark_line "${start}") + 1 ))
+  local end_bookmark=$(( $(get_bookmark_line "${end}") - 4 ))
+
+  log [${output}] [bookmark] ${start_bookmark},${end_bookmark}
+
+  sed -n "${start_bookmark},${end_bookmark}{p;${end_bookmark}q}" dark-sun.bookmarks \
+    | awk '/^BookmarkLevel: / {$2 -= 1} /^BookmarkPageNumber: / {$2 -= '"$(( ${start} - 1 ))"'} { print $0 }' \
+    > "${output}.bookmarks"
+
+  log [${output}] Created bookmarks
+}
+
 function split_pdf() {
   local target="${1}"
   local start="${2}"
-  local end="${3}"
+  local end=$(get_end_bookmark "${3}")
   local output="${4}"
 
   log [${output}] Start bare split
 
-  pdftk "${target}.pdf" cat "${start}-${end}" output "${output}_bare.pdf" &
-  sed -n "${start_bookmark},${end_bookmark}{p;${end_bookmark}q}" dark-sun.bookmarks \
-    | awk '/^BookmarkLevel: / {$2 -= 1} /^BookmarkPageNumber: / {$2 -= '"$(( ${start} - 1 ))"'} { print $0 }' \
-    > "${output}.bookmarks" &
-  wait
+  pdftk "${target}.pdf" cat "${start}-${end}" output "${output}_bare.pdf"
   mv "${output}_bare.pdf" "${output}.pdf"
+
   log [${output}] Finish bare split
 }
 
 TARGET="${1}"
+START="${2}"
+END="${3}"
 OUTPUT="${TARGET}_${4}"
 
-log [${output}] Get bookmark information
+create_bookmarks "${OUTPUT}" "${START}" "${END}" &
+split_pdf "${TARGET}" "${START}" "${END}" "${OUTPUT}" &
+wait
 
-START=$(( $(get_bookmark_line "${2}") + 1 ))
-END=$(get_end_bookmark "${3}")
-
-split_pdf "${TARGET}" "${START}" "${END}" "${OUTPUT}"
 ./scripts/create-low-res.sh ${OUTPUT}
-./scripts/optimize_pdf.sh ${OUTPUT}_low-res ${OUTPUT} ${OUTPUT}
+./scripts/optimize-pdf.sh ${OUTPUT}_low-res ${OUTPUT} ${OUTPUT}
 
 log [${OUTPUT}] Finish
